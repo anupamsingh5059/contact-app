a78313d3e16929ce75a9a4cfde3dba1466cfb30f
anupamsingh5059 <anupamsingh505011@gmail.com>
2025-09-12 16:20:08 +0530

    updated about.ejs table

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+
+
 import express from "express";
+const app = express();
 import mongoose, { connect, connections } from "mongoose";
 
 
@@ -10,13 +13,17 @@ import Contact from "./models/contacts.models.js";
 
 mongoose.connect("mongodb://127.0.0.1:27017/contacts-crud")
 .then(() => console.log("Database connection Success"))
-const app = express();
+
 // Middleware
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended:false}))
-app.use(express.static("public"))
+
+// app.use(express.static("public"))
+
+app.use(express.static("public"));
+
 // Route
 app.get('/', async (req, res)=> {
 
@@ -25,23 +32,44 @@ app.get('/', async (req, res)=> {
    res.render("home", {contacts:contacts})
 })
 
-app.get('/show-contact', (req, res)=> {
-  res.render("show-contact")
-})
+// app.get('/show-contact/:id', (req, res)=> {
+//   res.render("show-contact")
+// })
+
+app.get('/show-contact/:id', async (req, res) => {
+  try {
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      return res.status(404).send("Contact not found");
+    }
+    res.render("show-contact", { contact });
+  } catch (error) {
+    res.status(500).send("Error loading contact");
+  }
+});
+
 app.get('/add-contact', (req, res)=> {
     res.render("add-contact")
 })
-app.post('/add-contact', (req, res)=> {
+app.post('/add-contact/:id', (req, res)=> {
     
     
 })
-app.get('/update-contact', (req, res)=>{
-   res.render("update-contact")
-})
-app.post('/update-contact', (req, res)=>{
+app.get('/update-contact/:id', async (req, res) => {
+  try {
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) return res.status(404).send("Contact not found");
+
+    res.render("update-contact", { contact });
+  } catch (err) {
+    res.status(500).send("Error loading contact");
+  }
+});
+
+app.post('/update-contact/:id', (req, res)=>{
     
 })
-app.get('/delete-contact', (req, res)=>{
+app.get('/delete-contact/:id', (req, res)=>{
     
 })
 
